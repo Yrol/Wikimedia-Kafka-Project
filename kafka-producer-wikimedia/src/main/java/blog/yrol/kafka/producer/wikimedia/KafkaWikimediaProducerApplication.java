e5b59c9cddc9bd6a9d06b50ef82e9503b43e0c2c
Yrol <yrollgayanth@yahoo.com>
2023-08-30 22:42:23 +1000

    Setting up the base project

---
+++ kafka-producer-wikimedia/src/main/java/blog/yrol/kafka/producer/wikimedia/KafkaWikimediaProducerApplication.java
@@ -0,0 +1,25 @@
+package blog.yrol.kafka.producer.wikimedia;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = "blog.yrol")
+public class KafkaWikimediaProducerApplication implements CommandLineRunner {
+
+    public static final Logger LOG = LoggerFactory.getLogger(KafkaWikimediaProducerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWikimediaProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("App started");
+    }
+}
